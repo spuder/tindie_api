f7a888d14ab96be67537f82bff4363f0769b621d
Spencer Owen <owenspencer@gmail.com>
2024-12-17 21:07:07 -0700

    Add tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,6 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "tindie_api"
 
 require "minitest/autorun"
+
+require 'dotenv'
+Dotenv.load
--- test/tindie_api_test.rb
+++ test/tindie_api_test.rb
@@ -1,11 +1,47 @@
 require "test_helper"
+require 'webmock/minitest'
 
 class TindieApiTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::TindieApi::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def setup
+    @username = ENV['TINDIE_USERNAME']
+    @api_key = ENV['TINDIE_API_KEY']
+    @api = TindieApi::TindieOrdersAPI.new(@username, @api_key)
   end
+
+  def test_initialize
+    assert_equal @username, @api.instance_variable_get(:@usr)
+    assert_equal @api_key, @api.instance_variable_get(:@api)
+    assert_equal({ false => nil, true => nil, nil => nil }, @api.instance_variable_get(:@cache))
+  end
+
+  def test_get_orders_json
+    # puts "TINDIE_USERNAME: #{ENV['TINDIE_USERNAME']}"
+    # puts "TINDIE_API_KEY: #{ENV['TINDIE_API_KEY']}"
+    stub_request = stub_request(:get, "https://www.tindie.com/api/v1/order/?format=json&api_key=#{@api_key}&username=#{@username}")
+    stub_request.to_return(body: '{"orders": []}')
+
+    response = @api.get_orders_json
+    assert_equal({"orders" => []}, response)
+  end
+
+  def test_get_orders_json_with_shipped
+    stub_request = stub_request(:get, "https://www.tindie.com/api/v1/order/?format=json&api_key=#{@api_key}&username=#{@username}&shipped=true")
+    stub_request.to_return(body: '{"orders": []}')
+  
+    response = @api.get_orders_json(true)
+    assert_equal({"orders"=>[]}, response)
+  end
+
+  def test_get_orders_json_with_error
+    stub_request = stub_request(:get, "https://www.tindie.com/api/v1/order/?format=json&api_key=#{@api_key}&username=#{@username}")
+    stub_request.to_return(status: 500, body: 'Error')
+  
+    assert_raises(JSON::ParserError) { @api.get_orders_json }
+  end
+
+
 end
